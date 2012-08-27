7d8a8e643565b2e38aa0f80ebb8289adf3d1c720
Chris Adams <chris@improbable.org>
2012-08-27 13:20:07 -0400

    unicode-characters: significant performance boost
    Changing the I/O to always be UTF-8 bytestrings and avoiding repeated encoding
    conversions is good for something north of 5x performance boost on Python 3.2
    and PyPy 1.9. Wrapping the entire block in main() is also good for PyPy

--- bin/unicode-characters.py
+++ bin/unicode-characters.py
@@ -2,29 +2,55 @@
 # encoding: utf-8
 from __future__ import print_function, unicode_literals
 
-from unicodedata import category, name
+from unicodedata import category, name, normalize
 from binascii import hexlify
 import fileinput
-import re
+import os
 import sys
 
 MAJOR_VERSION = sys.version_info[0]
 
 CACHE = {}
 
-print("%8s\t%8s\t%s\t%s" % ("Char", "UTF8 Hex", "Category", "Name"))
-print("-" * 72)
 
-for line in fileinput.input():
-    if MAJOR_VERSION < 3:
-        line = line.decode("utf-8")
-    for char in line:
-        try:
-            cat, display_char, utf8_hex = CACHE[char]
-        except KeyError:
-            cat = category(char)
-            display_char = char.encode("unicode_escape").decode("ascii")
-            utf8_hex = hexlify(char.encode("utf8")).decode("ascii")
-            CACHE[char] = cat, display_char, utf8_hex
+# This is a bit gnarly but what we're trying to do is avoid a *MASSIVE* penalty
+# endlessly encoding and reencoding Unicode strings for output. This is
+# complicated by the fact that Python 2.x does not perform output encoding
+# automatically when piped so we end up needing to re-open the file-handle so
+# we can portably guarantee that our "stdout" object will be a simple file
+# to which we can directly write byte-strings. This requires us to carefully
+# encode output but that's worth a 500% boost on Python 3…
+if MAJOR_VERSION < 3:
+    stdout = os.fdopen(sys.stdout.fileno(), 'wb', 65536)
+else:
+    stdout = sys.stdout.buffer
 
-        print(display_char, utf8_hex, cat, name(char, "<UNKNOWN>"), sep="\t")
+
+def main():
+    stdout.write(("%8s\t%8s\t%s\t%s\n" % ("Char", "UTF8 Hex", "Category",
+                                          "Name")).encode("utf-8"))
+    stdout.write(b"-" * 72)
+    stdout.write(b"\n")
+    stdout.flush()
+
+    for line in fileinput.input():
+        if MAJOR_VERSION < 3:
+            line = line.decode("utf-8")
+
+        for char in normalize("NFKC", line):
+            # Since a given character can occur many times in our input, we'll
+            # precompute everything including the concatenation and UTF-8
+            # encoding:
+            try:
+                l = CACHE[char]
+            except KeyError:
+                cat = category(char).encode("utf-8")
+                utf8_hex = hexlify(char.encode("utf8"))
+                char_name = name(char, "<UNKNOWN>").encode("utf-8")
+                CACHE[char] = l = char.encode("utf-8") + b"\t" + utf8_hex \
+                                  + b"\t" + cat + b"\t" + char_name + b"\n"
+
+            stdout.write(l)
+
+if __name__ == "__main__":
+    main()
