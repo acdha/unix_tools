f106d4f24faa34015a19fdb005d40d0135989c32
Chris Adams <chris@improbable.org>
2012-08-27 11:51:34 -0400

    unicode-characters.py: now supports both Python 2 & 3

--- bin/unicode-characters.py
+++ bin/unicode-characters.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python
 # encoding: utf-8
+from __future__ import print_function, unicode_literals
 
+from unicodedata import category, name
+from binascii import hexlify
 import fileinput
-import unicodedata
 import re
+import sys
 
-print u"%8s\t%8s\t%s\t%s" % ("Char", "UTF8 Hex", "Category", "Name")
-print "-" * 72
+MAJOR_VERSION = sys.version_info[0]
+
+CACHE = {}
+
+print("%8s\t%8s\t%s\t%s" % ("Char", "UTF8 Hex", "Category", "Name"))
+print("-" * 72)
 
 for line in fileinput.input():
-    for char in line.decode("utf-8"):
-        category = unicodedata.category(char)
-        display_char = char.encode("unicode_escape")
-        utf8_hex = char.encode("utf8").encode("hex_codec")
+    if MAJOR_VERSION < 3:
+        line = line.decode("utf-8")
+    for char in line:
+        try:
+            cat, display_char, utf8_hex = CACHE[char]
+        except KeyError:
+            cat = category(char)
+            display_char = char.encode("unicode_escape").decode("ascii")
+            utf8_hex = hexlify(char.encode("utf8")).decode("ascii")
+            CACHE[char] = cat, display_char, utf8_hex
 
-        print u"%8s\t%8s\t%8s\t%s" % (display_char, utf8_hex, category,
-                                    unicodedata.name(char, "<UNKNOWN>"))
+        print(display_char, utf8_hex, cat, name(char, "<UNKNOWN>"), sep="\t")
