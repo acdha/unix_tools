73fc93d6f5e81d4db75af6abeac55ef65d2085ba
Chris Adams <chris@improbable.org>
2012-10-24 15:59:30 -0400

    Basic DNS comparison utility
    This is handy for validating DNS migrations

---
+++ bin/compare-dns.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+"""Compare DNS records across multiple servers
+
+Usage:
+
+    compare-dns.py --server=ns1.example.com example.org:SOA example.org
+
+You must specify at least one server and one record to check.
+The query type is assume to be A if it does not end with a colon-separated
+record type
+"""
+
+from collections import defaultdict
+import socket
+import sys
+
+import optparse
+
+from dns import resolver
+from dns.resolver import NoAnswer
+
+RDTYPE_SORT_WEIGHT = {"SOA": -3, "NS": -2, "MX": -1}
+
+
+def get_records_from_server(server, records):
+    r = resolver.Resolver()
+    r.nameservers = [socket.gethostbyname(server)]
+    r.search = []
+
+    for qname, rdtype in records:
+        try:
+            for answer in r.query(qname, rdtype=rdtype):
+                yield qname, rdtype, answer.to_text()
+        except NoAnswer:
+            yield qname, rdtype, None
+
+
+def normalize_record(i):
+    if ':' in i:
+        return i.split(":", 1)
+    else:
+        return i, "A"
+
+
+def query_sort_key(qname, rdtype):
+    """Sort DNS responses by record type & name"""
+    return RDTYPE_SORT_WEIGHT.get(rdtype, rdtype), qname
+
+
+def main():
+    parser = optparse.OptionParser(__doc__.strip())
+    parser.add_option('--server', type="str", dest="servers", action="append",
+                      metavar="SERVER",
+                      help="hostname of the DNS server to check (repeatable)")
+
+    (options, records) = parser.parse_args()
+
+    if not records or not options.servers:
+        parser.print_usage()
+        sys.exit()
+
+    records = map(normalize_record, records)
+
+    results = defaultdict(lambda: defaultdict(list))
+
+    for server in options.servers:
+        print "Querying %s" % server
+        for qname, rdtype, resp in get_records_from_server(server, records):
+            results[qname, rdtype][server].append(resp)
+    print
+
+    for query, responses in sorted(results.items(), key=lambda i: query_sort_key(*i[0])):
+        print "%s:%s" % query
+        for server, answers in sorted(responses.items(), key=lambda i: i[0]):
+            print "\t%s:" % server
+            for answer in sorted(answers):
+                print "\t\t\t\t%s" % answer
+        print
+
+if __name__ == "__main__":
+    main()
